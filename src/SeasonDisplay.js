f55b981344e530cf73fc4ef16f23ac21f213d7cf
Kritapas Nimtrakul <kritapas333@hotmail.com>
2019-06-20 22:48:15 -0400

    Rework season app with react hooks

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,36 @@
+import './SeasonDisplay.css';
+import React from 'react';
+
+const seasonConfig = {
+  summer: {
+    text: 'lets go hiking',
+    iconName: "sun"
+  },
+  winter: {
+    text: 'uh, it is chilly',
+    iconName: "snowflake"
+  }
+};
+
+const getSeason = (lat, month) => {
+  if(month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  }else{
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const {text, iconName} = seasonConfig[season] // {text, iconName}
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
+};
+
+export default SeasonDisplay;
